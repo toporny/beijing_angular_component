ed2979bc924aaa0c79603594a0c5ec441c8af70f
toporny <toporny@tlen.pl>
2016-09-01 20:05:54 +0100

    flags

--- public_html/directives/medals.directive.js
+++ public_html/directives/medals.directive.js
@@ -1,23 +1,3 @@
-(function() {
-  angular.module('challengeApp')
-  .controller('MedalsController', MedalsController);
-
-  MedalsController.$inject = ['$scope'];
-
-  function MedalsController ($scope ) {
-    vm = this;
-    vm.data = {
-      price : '0'
-    };
-    that = vm;
- 
-      that.data.price = '123';
- 
-  }
-})();
-
-
-
 (function () {
     var componentName = 'medalStats',
         templateUrl = 'directives/medals.tpl.html';
@@ -26,11 +6,12 @@
         .directive(componentName, component)
         .controller(componentName, medalCtrl);
 
-    // for unit testing: $controller('intro') === new instance of 'medalCtrl'
+    // for unit testing: $controller('medal') === new instance of 'medalCtrl'
 
     function component() {
         return {
-            scope: { },
+            scope: {
+            },
             controller: medalCtrl,
             controllerAs: '$ctrl',
             bindToController: true,
@@ -38,55 +19,36 @@
         };
     }
 
-    medalCtrl.$inject = ['$http', 'customCommunication' , 'country_codes'];
+    medalCtrl.$inject = ['$http', 'customCommunication'];
 
-    function medalCtrl($http, customCommunication, country_codes) {
-        // {
-        //   "athlete": "KOGO, Micah",
-        //   "country": "KEN",
-        //   "sex": "Men",
-        //   "event": "10000m",
-        //   "medal": "Bronze"
-        // },
-
+    function medalCtrl($http, customCommunication) {
         var $ctrl = this;
         $ctrl.init = init;
-
-        function getJsonData(){
-
+        $ctrl.sortChange = sortChange;
+        
+        function sortChange(sortby){
+          if (sortby == $ctrl.sortby) {
+            $ctrl.reverse = !$ctrl.reverse;
+            return;
+          }
+          $ctrl.sortby = sortby;
         }
 
         function init(){
-
-          $ctrl.httpStatus = 1;
-
+          $ctrl.httpStatus = 0;
+          $ctrl.sortby = 'gold';
+          $ctrl.reverse = true;
+          
           customCommunication.getJsonData()
           .then(function (response) {
-            for (i=0; i<response.length; i++) {
-              response[i].country_name = country_codes[response[i].country];
-            }
-            console.log('response', response);
-            //this.push(key + ': ' + value);
-            return response;
+            $ctrl.medalData = response;
+            $ctrl.httpStatus = 1;
           })
           .catch(function(fallback) {
+            $ctrl.httpStatus = -1;
             console.warn('error communication:', fallback);
           });
-
-          // $http({
-          //   method: 'GET',
-          //   url: 'athletic_medalists.json.txt'
-          // }).then(function successCallback(response) {
-          //   console.log(response);
-          //   $ctrl.httpStatus = -1; // called asynchronously if an error occurs  or server returns response with an error status.
-          //   $ctrl.errorDescription = 'Problem with getting JSON data. Check your internet connection.';
-          // }, function errorCallback(response) {
-          //   $ctrl.httpStatus = -1; // called asynchronously if an error occurs  or server returns response with an error status.
-          //   $ctrl.errorDescription = 'Problem with getting JSON data. Check your internet connection.';
-          //   console.error($ctrl.errorDescription + ' from url: ' +jsonEndpoint);
-          // });
         }
-
 
         activate();
 
--- public_html/services/get-json-data.service.js
+++ public_html/services/get-json-data.service.js
@@ -3,10 +3,12 @@
         .module('challengeApp')
         .factory('customCommunication', customCommunication);
 
-    customCommunication.$inject = ['$q', '$http', 'jsonEndpoint'];
+    customCommunication.$inject = ['$q', '$http', 'jsonEndpoint', 'country_codes'];
 
-    function customCommunication($q, $http, jsonEndpoint) {
+    function customCommunication($q, $http, jsonEndpoint, country_codes) {
+        
         // interface
+
         var service = {
             getJsonData: getJsonData
         };
@@ -17,9 +19,54 @@
 
         function getJsonData(command) {
           return $http.get(jsonEndpoint).then(function(response) {
-            //console.log('response',response);
             if (typeof response.data === "object") {
-              return response.data;
+              aCountriesArray = [];
+
+              // make table with all winning countries
+              for (i=0; i<response.data.length; i++) {
+                if (aCountriesArray.indexOf(response.data[i].country) == -1) {
+                  aCountriesArray.push(response.data[i].country);
+                }
+              }
+
+              // create countries object with empty medal properities
+              oCountries = {};
+              
+              for (i=0; i<aCountriesArray.length; i++) {
+                
+                console.log(aCountriesArray[i]);
+                //console.log(country_codes[aCountriesArray[i]]);
+                oCountries[aCountriesArray[i]] = {
+                  country_code: aCountriesArray[i],
+                  country_name: country_codes[aCountriesArray[i]]['state'],
+                  flag: country_codes[aCountriesArray[i]]['flag'],
+                  gold:0,
+                  silver:0,
+                  bronze:0
+                }
+              }
+
+              // increment medals counters
+              for (i=0; i<response.data.length; i++) {
+                switch (response.data[i].medal) {
+                  case 'Gold': oCountries[response.data[i].country].gold++;
+                  break;
+                  case 'Silver':  oCountries[response.data[i].country].silver++;
+                  break;
+                  case 'Bronze': oCountries[response.data[i].country].bronze++;
+                  break;
+                }
+              }
+
+              // prepare return array
+              aReturn = new Array();
+
+              angular.forEach(oCountries, function(value, key) {
+                aReturn.push(value);
+              });
+
+              return aReturn;
+
             } else {
               return $q.reject(response.data);
             }
